5807c0f95cc2e81974de83f3baa78c0b78d66212
Jocelyn Boyd <jocelynboyd@pursuit.org>
2020-04-09 15:29:15 -0400

    setup calculator UI via storyboard

---
+++ calculator/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Jocelyn Boyd on 4/9/20.
+//  Copyright © 2020 Jocelyn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  //MARK: Digit Buttons
+  @IBOutlet weak var zerobutton: UIButton!
+  @IBOutlet weak var oneButton: UIButton!
+  @IBOutlet weak var twoButton: UIButton!
+  @IBOutlet weak var threeButton: UIButton!
+  @IBOutlet weak var fourButton: UIButton!
+  @IBOutlet weak var fiveButtons: UIButton!
+  @IBOutlet weak var sixButton: UIButton!
+  @IBOutlet weak var sevenButton: UIButton!
+  @IBOutlet weak var eightButton: UIButton!
+  @IBOutlet weak var nineButton: UIButton!
+  
+  
+  
+//MARK: Operation Buttons
+  @IBOutlet weak var clearButton: UIButton!
+  @IBOutlet weak var multiplicationButton: UIButton!
+  @IBOutlet weak var subtractionButton: UIButton!
+  @IBOutlet weak var plusButton: UIButton!
+  @IBOutlet weak var decimalButton: UIButton!
+  @IBOutlet weak var equalButton: UIButton!
+  
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
